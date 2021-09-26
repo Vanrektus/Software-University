e3f723c35568bfa5e689d7354c09cdb45407f39d
Vanrektus <mumiata123@abv.bg>
2021-09-26 22:03:29 +0300

    Add files via upload
    Add files via upload

---
+++ C# Advanced/05. Functional Programming/Exercise/PartyReservationFilter/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PartyReservationFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, string, bool> startsWith = (x, y) => x.StartsWith(y);
+            Func<string, string, bool> endsWith = (x, y) => x.EndsWith(y);
+            Func<string, int, bool> length = (x, y) => x.Length == y;
+            Func<string, string, bool> contains = (x, y) => x.Contains(y);
+
+            List<string> result = new List<string>(guests);
+            List<string> filtered = new List<string>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Print")
+                {
+                    break;
+                }
+
+                switch (command[1])
+                {
+                    case "Starts with":
+                        filtered = guests
+                            .Where(x => startsWith(x, command[2]))
+                            .ToList();
+                        break;
+                    case "Ends with":
+                        filtered = guests
+                            .Where(x => endsWith(x, command[2]))
+                            .ToList();
+                        break;
+                    case "Length":
+                        filtered = guests
+                            .Where(x => length(x, int.Parse(command[2])))
+                            .ToList();
+                        break;
+                    case "Contains":
+                        filtered = guests
+                            .Where(x => contains(x, command[2]))
+                            .ToList();
+                        break;
+                }
+
+                switch (command[0])
+                {
+                    case "Add filter":
+                        result.RemoveAll(x => filtered.Contains(x));
+                        break;
+                    case "Remove filter":
+                        result.AddRange(filtered);
+                        result = result
+                            .Distinct()
+                            .ToList();
+                        break;
+                }
+            }
+
+            guests.RemoveAll(x => !result.Contains(x));
+            Console.WriteLine(string.Join(" ", guests));
+        }
+    }
+}
---
+++ C# Advanced/05. Functional Programming/Exercise/PredicateParty/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, string, bool> startsWith = (x, y) => x.StartsWith(y);
+            Func<string, string, bool> endsWith = (x, y) => x.EndsWith(y);
+            Func<string, int, bool> length = (x, y) => x.Length == y;
+
+            Func<List<string>, List<string>, List<string>> doublePeople = (x, y) =>
+            {
+                foreach (var doubled in y)
+                {
+                    for (int i = 0; i < x.Count; i++)
+                    {
+                        if (i < x.Count)
+                        {
+                            if (x[i] == doubled)
+                            {
+                                x.Insert(i, doubled);
+                                i++;
+                            }
+                        }
+                    }
+                }
+
+                return x;
+            };
+
+            List<string> filteredGuests = new List<string>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Party!")
+                {
+                    break;
+                }
+
+                switch (command[1])
+                {
+                    case "StartsWith":
+                        filteredGuests = guests
+                            .Where(x => startsWith(x, command[2]))
+                            .Distinct()
+                            .ToList();
+                        break;
+                    case "EndsWith":
+                        filteredGuests = guests
+                            .Where(x => endsWith(x, command[2]))
+                            .Distinct()
+                            .ToList();
+                        break;
+                    case "Length":
+                        filteredGuests = guests
+                            .Where(x => length(x, int.Parse(command[2])))
+                            .Distinct()
+                            .ToList();
+                        break;
+                }
+
+                switch (command[0])
+                {
+                    case "Double":
+                        guests = doublePeople(guests, filteredGuests);
+                        break;
+                    case "Remove":
+                        guests = guests
+                            .Where(x => !filteredGuests.Contains(x))
+                            .ToList();
+                        break;
+                }
+            }
+
+            Console.WriteLine((guests.Count > 0) ? $"{String.Join(", ", guests)} are going to the party!" : "Nobody is going to the party!");
+        }
+    }
+}
