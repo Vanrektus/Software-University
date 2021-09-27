f520b968b56277a639bcaeec9035a07736bb8256
Vanrektus <mumiata123@abv.bg>
2021-09-28 01:16:01 +0300

    Add files via upload
    Add files via upload

---
+++ C# Advanced/06. Defining classes/Lab/Special Cars/CarManufacturer/Engine.cs
@@ -0,0 +1,29 @@
+﻿namespace CarManufacturer
+{
+    public class Engine
+    {
+        //---------------------------Fields---------------------------
+        private int horsePower;
+        private double cubicCapacity;
+
+        //---------------------------Properties---------------------------
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+
+        public double CubicCapacity
+        {
+            get { return cubicCapacity; }
+            set { cubicCapacity = value; }
+        }
+
+        //---------------------------Constructors---------------------------
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+    }
+}
