50bf0e1643d0eb1a3fb4d8c466a03ca875f62e96
Vanrektus <mumiata123@abv.bg>
2021-10-04 21:32:43 +0300

    Add files via upload
    Add files via upload

---
+++ C# Advanced/08. Generics/Lab/BoxOfT/Box.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private readonly List<T> elements; // = new List<T>();
+
+        public int Count
+        {
+            get
+            {
+                return elements.Count;
+            }
+        }
+
+        public Box()
+        {
+            elements = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            T result = elements.LastOrDefault();
+            elements.Remove(result);
+
+            return result;
+        }
+    }
+}
---
+++ C# Advanced/08. Generics/Lab/GenericScale/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> intScale = new EqualityScale<int>(3, 3);
+            EqualityScale<string> stringScale = new EqualityScale<string>("Pesho", "Pesho");
+            EqualityScale<string> falseStringScale = new EqualityScale<string>("Pesho", "Gosho");
+
+            Console.WriteLine(intScale.AreEqual());
+            Console.WriteLine(stringScale.AreEqual());
+            Console.WriteLine(falseStringScale.AreEqual());
+        }
+    }
+}
