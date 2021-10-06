698230372773ec55dd0a710bacaf709af7fb6ea2
Vanrektus <mumiata123@abv.bg>
2021-10-06 22:00:12 +0300

    Add files via upload
    Add files via upload

--- C# Advanced/09. Iterators and Comparators/Lab/LibraryIterator/Book.cs
+++ C# Advanced/09. Iterators and Comparators/Lab/LibraryIterator/Book.cs
@@ -5,11 +5,11 @@ namespace IteratorsAndComparators
     public class Book
     {
         //---------------------------Properties---------------------------
-        public string Title { get; set; }
+        public string Title { get; private set; }
 
-        public int Year { get; set; }
+        public int Year { get; private set; }
 
-        public IEnumerable<string> Authors { get; set; }
+        public IEnumerable<string> Authors { get; private set; }
 
         //---------------------------Constructors---------------------------
         public Book(string title, int year, params string[] authors)
