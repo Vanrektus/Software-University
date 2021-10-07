7dd4118f8b7d567cb8615f7d104fd7af72d740cc
Vanrektus <mumiata123@abv.bg>
2021-10-07 22:19:55 +0300

    Add files via upload
    Add files via upload

---
+++ C# Advanced/06. Defining classes/Exercise/SoftUniParking/Car.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        //---------------------------Properties---------------------------
+        public string Make { get; private set; }
+
+        public string Model { get; private set; }
+
+        public int HorsePower { get; private set; }
+
+        public string RegistrationNumber { get; private set; }
+
+        //---------------------------Constructors---------------------------
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        //---------------------------Methods---------------------------
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"HorsePower: {this.HorsePower}");
+            sb.Append($"RegistrationNumber: {this.RegistrationNumber}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# Advanced/06. Defining classes/Exercise/SoftUniParking/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SoftUniParking
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car firstCar = new Car("Skoda", "Fabia", 65, "CC1856BG");
+            Car secondCar = new Car("Audi", "A3", 110, "EB8787MN");
+
+            Console.WriteLine(firstCar.ToString());
+
+            Parking parking = new Parking(5);
+
+            Console.WriteLine(parking.AddCar(firstCar));
+
+            Console.WriteLine(parking.AddCar(firstCar));
+
+            Console.WriteLine(parking.AddCar(secondCar));
+
+            Console.WriteLine(parking.GetCar("EB8787MN").ToString());
+
+            Console.WriteLine(parking.RemoveCar("EB8787MN"));
+
+            Console.WriteLine(parking.Count);
+        }
+    }
+}
