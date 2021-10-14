c8bc7798492b76ec87f1c075afed306f4361930d
Vanrektus <mumiata123@abv.bg>
2021-10-14 15:16:00 +0300

    Add files via upload
    Add files via upload

---
+++ C# Advanced/13. Recursion Introduction/StackOverflowException/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StackOverflowException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackOverflowSux();
+        }
+
+        static unsafe void StackOverflowSux()
+        {
+            int x = 5;
+
+            Console.WriteLine((int)&x);
+
+            StackOverflowSux();
+        }
+    }
+}
