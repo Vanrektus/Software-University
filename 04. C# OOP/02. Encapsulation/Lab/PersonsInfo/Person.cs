c76b70e0305bcaefb9001942b6212fb34f63f925
Vanrektus <mumiata123@abv.bg>
2021-10-19 16:30:39 +0300

    Add files via upload
    Add files via upload

---
+++ 04. C# OOP/02. Encapsulation/Lab/PersonsInfo/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        //---------------------------Properties---------------------------
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        //---------------------------Constructors---------------------------
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        //---------------------------Methods---------------------------
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+    }
+}
---
+++ 04. C# OOP/02. Encapsulation/Lab/PersonsInfo/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentPerson = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = currentPerson[0];
+                string lastName = currentPerson[1];
+                int age = int.Parse(currentPerson[2]);
+
+                Person person = new Person(firstName, lastName, age);
+                people.Add(person);
+            }
+
+            people.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+
+            //foreach (Person currentPerson in people.OrderBy(p => p.FirstName).ThenBy(p => p.Age))
+            //{
+            //    Console.WriteLine(currentPerson);
+            //}
+        }
+    }
+}
