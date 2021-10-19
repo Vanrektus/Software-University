e46469af89218bfa8735b75f42c7a375d94ff071
Vanrektus <mumiata123@abv.bg>
2021-10-19 18:22:27 +0300

    Add files via upload
    Add files via upload

---
+++ 04. C# OOP/02. Encapsulation/Lab/Team/Person.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        //---------------------------Fields---------------------------
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        //---------------------------Properties---------------------------
+        public string FirstName
+        {
+            get { return firstName; }
+
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+
+            set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+
+                salary = value;
+            }
+        }
+
+        //---------------------------Constructors---------------------------
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+            : this(firstName, lastName, age)
+        {
+            this.Salary = salary;
+        }
+
+        //---------------------------Methods---------------------------
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            Salary += Salary * percentage / 100;
+        }
+    }
+}
---
+++ 04. C# OOP/02. Encapsulation/Lab/Team/Team.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        //---------------------------Fields---------------------------
+        private readonly string name;
+        private readonly List<Person> firstTeam;
+        private readonly List<Person> reserveTeam;
+
+        //---------------------------Properties---------------------------
+        public IReadOnlyCollection<Person> FirstTeam
+        { 
+            get { return this.firstTeam.AsReadOnly(); }
+        }
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+
+        //---------------------------Constructors---------------------------
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        //---------------------------Methods---------------------------
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
