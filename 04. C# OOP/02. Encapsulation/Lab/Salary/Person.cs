6bc61441858697d2a3e90bc9d8b737438cc2d4ca
Vanrektus <mumiata123@abv.bg>
2021-10-19 17:57:47 +0300

    Add files via upload
    Add files via upload

---
+++ 04. C# OOP/02. Encapsulation/Lab/Salary/Person.cs
@@ -0,0 +1,44 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        //---------------------------Properties---------------------------
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public decimal Salary { get; private set; }
+
+        //---------------------------Constructors---------------------------
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+            : this(firstName, lastName, age)
+        {
+            this.Salary = salary;
+        }
+
+        //---------------------------Methods---------------------------
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            Salary += Salary * percentage / 100;
+        }
+    }
+}
