9c2f23d83c8a45557b604b4f1242427a0cb2c6a4
Vanrektus <mumiata123@abv.bg>
2021-10-22 18:36:47 +0300

    Add files via upload
    Add files via upload

---
+++ 03. C# Advanced/11. Exam Prep/February2021/TheFightForGondor/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheFightForGondor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWaves = int.Parse(Console.ReadLine());
+            Queue<int> plates = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+            Stack<int> orcs = new Stack<int>();
+
+            orcs = TheFight(numberOfWaves, plates, orcs);
+
+            PrintOutput(plates, orcs);
+        }
+
+        private static Stack<int> TheFight(int numberOfWaves, Queue<int> plates, Stack<int> orcs)
+        {
+            for (int wave = 1; wave <= numberOfWaves; wave++)
+            {
+                orcs = new Stack<int>(Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse));
+
+                if (wave % 3 == 0)
+                {
+                    plates.Enqueue(int.Parse(Console.ReadLine()));
+                }
+
+                while (plates.Count > 0 && orcs.Count > 0)
+                {
+                    int currentPlate = plates.Peek();
+                    int currentOrc = orcs.Peek();
+
+                    if (currentPlate > currentOrc)
+                    {
+                        currentPlate -= currentOrc;
+
+                        orcs.Pop();
+
+                        plates.Dequeue();
+                        plates.Enqueue(currentPlate);
+
+                        for (int i = 0; i < plates.Count - 1; i++)
+                        {
+                            plates.Enqueue(plates.Dequeue());
+                        }
+                    }
+                    else if (currentPlate < currentOrc)
+                    {
+                        currentOrc -= currentPlate;
+
+                        orcs.Pop();
+                        orcs.Push(currentOrc);
+
+                        plates.Dequeue();
+                    }
+                    else if (currentPlate == currentOrc)
+                    {
+                        plates.Dequeue();
+                        orcs.Pop();
+                    }
+                }
+
+                if (plates.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            return orcs;
+        }
+
+        private static void PrintOutput(Queue<int> plates, Stack<int> orcs)
+        {
+            if (plates.Count > 0)
+            {
+                Console.WriteLine("The people successfully repulsed the orc's attack.");
+                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
+            }
+            else
+            {
+                Console.WriteLine("The orcs successfully destroyed the Gondor's defense.");
+                Console.WriteLine($"Orcs left: {string.Join(", ", orcs)}");
+            }
+        }
+    }
+}
---
+++ 03. C# Advanced/11. Exam Prep/February2021/Warships/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+
+namespace Warships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfField = int.Parse(Console.ReadLine());
+            char[,] field = new char[sizeOfField, sizeOfField];
+
+            string[] attackCommands = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+            int firstPlayerShips = 0;
+            int secondPlaterShips = 0;
+
+            FieldFill(field, ref firstPlayerShips, ref secondPlaterShips);
+
+            int shipsDestroyed = Gameplay(sizeOfField, field, attackCommands, ref firstPlayerShips, ref secondPlaterShips);
+
+            PrintOutput(firstPlayerShips, secondPlaterShips, shipsDestroyed);
+        }
+
+        private static void FieldFill(char[,] field, ref int firstPlayerShips, ref int secondPlaterShips)
+        {
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    field[row, col] = char.Parse(input[col]);
+
+                    if (field[row, col] == '<')
+                    {
+                        firstPlayerShips++;
+                    }
+                    else if (field[row, col] == '>')
+                    {
+                        secondPlaterShips++;
+                    }
+                }
+            }
+        }
+
+        private static int Gameplay(int sizeOfField, char[,] field, string[] attackCommands, ref int firstPlayerShips, ref int secondPlaterShips)
+        {
+            int shipsDestroyed = 0;
+
+            for (int i = 0; i < attackCommands.Length; i++)
+            {
+                string[] splittedCommands = attackCommands[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int attackRow = int.Parse(splittedCommands[0]);
+                int attackCol = int.Parse(splittedCommands[1]);
+
+                if (attackRow < 0 || attackRow >= sizeOfField)
+                {
+                    continue;
+                }
+
+                if (attackCol < 0 || attackCol >= sizeOfField)
+                {
+                    continue;
+                }
+
+                if (field[attackRow, attackCol] == '*')//regular position
+                {
+                    continue;
+                }
+                else if (field[attackRow, attackCol] == '<')// first player
+                {
+                    field[attackRow, attackCol] = 'X';
+                    firstPlayerShips--;
+                    shipsDestroyed++;
+                }
+                else if (field[attackRow, attackCol] == '>')//second player
+                {
+                    field[attackRow, attackCol] = 'X';
+                    secondPlaterShips--;
+                    shipsDestroyed++;
+                }
+                else if (field[attackRow, attackCol] == '#')//mine
+                {
+                    field[attackRow, attackCol] = 'X';
+
+                    if (attackRow - 1 >= 0)
+                    {
+                        if (field[attackRow - 1, attackCol] == '<')
+                        {
+                            field[attackRow - 1, attackCol] = 'X';
+                            firstPlayerShips--;
+                            shipsDestroyed++;
+                        }
+                        else if (field[attackRow - 1, attackCol] == '>')
+                        {
+                            field[attackRow - 1, attackCol] = 'X';
+                            secondPlaterShips--;
+                            shipsDestroyed++;
+                        }
+                    }
+
+                    if (attackRow + 1 < sizeOfField)
+                    {
+                        if (field[attackRow + 1, attackCol] == '<')
+                        {
+                            field[attackRow + 1, attackCol] = 'X';
+                            firstPlayerShips--;
+                            shipsDestroyed++;
+                        }
+                        else if (field[attackRow + 1, attackCol] == '>')
+                        {
+                            field[attackRow + 1, attackCol] = 'X';
+                            secondPlaterShips--;
+                            shipsDestroyed++;
+                        }
+                    }
+
+                    if (attackCol - 1 >= 0)
+                    {
+                        if (field[attackRow, attackCol - 1] == '<')
+                        {
+                            field[attackRow, attackCol - 1] = 'X';
+                            firstPlayerShips--;
+                            shipsDestroyed++;
+                        }
+                        else if (field[attackRow, attackCol - 1] == '>')
+                        {
+                            field[attackRow, attackCol - 1] = 'X';
+                            secondPlaterShips--;
+                            shipsDestroyed++;
+                        }
+                    }
+
+                    if (attackCol + 1 < sizeOfField)
+                    {
+                        if (field[attackRow, attackCol + 1] == '<')
+                        {
+                            field[attackRow, attackCol + 1] = 'X';
+                            firstPlayerShips--;
+                            shipsDestroyed++;
+                        }
+                        else if (field[attackRow, attackCol + 1] == '>')
+                        {
+                            field[attackRow, attackCol + 1] = 'X';
+                            secondPlaterShips--;
+                            shipsDestroyed++;
+                        }
+                    }
+
+                    if (attackRow - 1 >= 0 && attackCol - 1 >= 0)
+                    {
+                        if (field[attackRow - 1, attackCol - 1] == '<')
+                        {
+                            field[attackRow - 1, attackCol - 1] = 'X';
+                            firstPlayerShips--;
+                            shipsDestroyed++;
+                        }
+                        else if (field[attackRow - 1, attackCol - 1] == '>')
+                        {
+                            field[attackRow - 1, attackCol - 1] = 'X';
+                            secondPlaterShips--;
+                            shipsDestroyed++;
+                        }
+                    }
+
+                    if (attackRow - 1 >= 0 && attackCol + 1 < sizeOfField)
+                    {
+                        if (field[attackRow - 1, attackCol + 1] == '<')
+                        {
+                            field[attackRow - 1, attackCol + 1] = 'X';
+                            firstPlayerShips--;
+                            shipsDestroyed++;
+                        }
+                        else if (field[attackRow - 1, attackCol + 1] == '>')
+                        {
+                            field[attackRow - 1, attackCol + 1] = 'X';
+                            secondPlaterShips--;
+                            shipsDestroyed++;
+                        }
+                    }
+
+                    if (attackRow + 1 < sizeOfField && attackCol - 1 >= 0)
+                    {
+                        if (field[attackRow + 1, attackCol - 1] == '<')
+                        {
+                            field[attackRow + 1, attackCol - 1] = 'X';
+                            firstPlayerShips--;
+                            shipsDestroyed++;
+                        }
+                        else if (field[attackRow + 1, attackCol - 1] == '>')
+                        {
+                            field[attackRow + 1, attackCol - 1] = 'X';
+                            secondPlaterShips--;
+                            shipsDestroyed++;
+                        }
+                    }
+
+                    if (attackRow + 1 < sizeOfField && attackCol + 1 < sizeOfField)
+                    {
+                        if (field[attackRow + 1, attackCol + 1] == '<')
+                        {
+                            field[attackRow + 1, attackCol + 1] = 'X';
+                            firstPlayerShips--;
+                            shipsDestroyed++;
+                        }
+                        else if (field[attackRow + 1, attackCol + 1] == '>')
+                        {
+                            field[attackRow + 1, attackCol + 1] = 'X';
+                            secondPlaterShips--;
+                            shipsDestroyed++;
+                        }
+                    }
+                }
+
+                if (firstPlayerShips <= 0)
+                {
+                    break;
+                }
+
+                if (secondPlaterShips <= 0)
+                {
+                    break;
+                }
+            }
+
+            return shipsDestroyed;
+        }
+
+        private static void PrintOutput(int firstPlayerShips, int secondPlaterShips, int shipsDestroyed)
+        {
+            if (firstPlayerShips <= 0)
+            {
+                Console.WriteLine($"Player Two has won the game! {shipsDestroyed} ships have been sunk in the battle.");
+            }
+            else if (secondPlaterShips <= 0)
+            {
+                Console.WriteLine($"Player One has won the game! {shipsDestroyed} ships have been sunk in the battle.");
+            }
+            else
+            {
+                Console.WriteLine($"It's a draw! Player One has {firstPlayerShips} ships left. Player Two has {secondPlaterShips} ships left.");
+            }
+        }
+    }
+}
