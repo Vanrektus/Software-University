cd3a7df9b7480a014f6fc9783bce08accf2468bb
Vanrektus <mumiata123@abv.bg>
2021-11-04 22:21:44 +0200

    Add files via upload
    Add files via upload

---
+++ 04. C# OOP/05. Exception Handling/CustomException/LectionOvertimeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomException
+{
+    public class LectionOvertimeException : ApplicationException
+    {
+        //---------------------------Properties---------------------------
+        public int Overtime { get; private set; }
+
+        //---------------------------Constructors---------------------------
+        public LectionOvertimeException(string message)
+            : base(message)
+        {
+
+        }
+
+        public LectionOvertimeException(string message, int overtime)
+            : base(message)
+        {
+            this.Overtime = overtime;
+        }
+    }
+}
---
+++ 04. C# OOP/05. Exception Handling/CustomException/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int lectionTime = int.Parse(Console.ReadLine());
+
+                CheckTime(lectionTime);
+            }
+            catch (LectionOvertimeException loe)
+            {
+                Console.WriteLine($"{loe.Message}, {loe.Overtime} minuti poveche");
+            }
+        }
+
+        private static void CheckTime(int lectionTime)
+        {
+            if (lectionTime > 180)
+            {
+                throw new LectionOvertimeException("Prestara se", lectionTime - 180);
+            }
+        }
+    }
+}
