fc85292ea5132aecbfdd6c2d614a974a19064fb4
Vanrektus <mumiata123@abv.bg>
2022-05-10 19:54:56 +0300

    Add files via upload
    Add files via upload

--- C# Web Development/07. C# DB - MS SQL/05. Subqueries and Joins/Lab/Lab Presentation Problems.sql
+++ C# Web Development/07. C# DB - MS SQL/05. Subqueries and Joins/Lab/Lab Presentation Problems.sql
@@ -1,39 +1,40 @@
 -- Problem: Addresses With Towns
 SELECT 
-	 e.FirstName,
-	e.LastName,
-	t.[Name] AS Town,
-	a.AddressText
-FROM Employees AS e
+	       e.FirstName,
+           e.LastName,
+	       t.[Name]  AS Town,
+	       a.AddressText
+      FROM Employees AS e
 INNER JOIN Addresses AS a ON e.AddressID = a.AddressID
-INNER JOIN Towns AS t ON a.TownID = t.TownID
-ORDER BY e.FirstName ASC, e.LastName ASC
+INNER JOIN Towns     AS t ON a.TownID = t.TownID
+  ORDER BY e.FirstName ASC, 
+		   e.LastName  ASC
 
 
 -- Problem: Salse Employees
 SELECT 
-	e.EmployeeID,
-	e.FirstName,
-	e.LastName,
-	d.[Name] AS DepartmentName
-FROM Employees AS e
-INNER JOIN Departments AS d ON e.DepartmentID = d.DepartmentID -- AND d.[Name] = 'Sales'
-WHERE d.[Name] = 'Sales'
-ORDER BY e.EmployeeID  ASC
+	       e.EmployeeID,
+	       e.FirstName,
+	       e.LastName,
+	       d.[Name]    AS DepartmentName
+      FROM Employees   AS e
+INNER JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+     WHERE d.[Name] = 'Sales'
+  ORDER BY e.EmployeeID ASC
 
 
 -- Problem: Employees Hired After
 SELECT 
-	 e.FirstName,
-	e.LastName,
-	e.HireDate,
-	d.[Name] AS DeptName
-FROM Employees AS e
+		   e.FirstName,
+		   e.LastName,
+		   e.HireDate,
+           d.[Name]    AS DeptName
+      FROM Employees   AS e
 INNER JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-WHERE 
-	e.HireDate > '1999-01-01' 
-	AND (d.[Name] IN ('Sales', 'Finance'))
-ORDER BY e.HireDate ASC
+     WHERE 
+	       e.HireDate > '1999-01-01' 
+	       AND (d.[Name] IN ('Sales', 'Finance'))
+  ORDER BY e.HireDate ASC
 
 
 -- Problem: Employee Summary
@@ -41,9 +42,9 @@ SELECT TOP (50)
 	       e.EmployeeID,
 	       CONCAT(e.FirstName, ' ', e.LastName) AS EmployeeName,
 	       CONCAT(m.FirstName, ' ', m.LastName) AS ManagerName,
-	       d.[Name] AS DepartmentName
-      FROM Employees AS e
- LEFT JOIN Employees AS m ON e.ManagerID = m.EmployeeID
+	       d.[Name]                             AS DepartmentName
+      FROM Employees   AS e
+ LEFT JOIN Employees   AS m ON e.ManagerID = m.EmployeeID
  LEFT JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
   ORDER BY e.EmployeeID
 
@@ -51,9 +52,10 @@ SELECT TOP (50)
 -- Problem: Min Average Salary
 SELECT 
        MIN(MinSal.AvgSal) AS MinAverageSalary
-  FROM (
-		 SELECT 
-		        AVG(Salary) AS AvgSal
-	       FROM Employees  
-	   GROUP BY DepartmentID
-	   ) AS MinSal
+  FROM 
+(
+	  SELECT 
+		     AVG(Salary) AS AvgSal
+	    FROM Employees  
+	GROUP BY DepartmentID
+) AS MinSal
